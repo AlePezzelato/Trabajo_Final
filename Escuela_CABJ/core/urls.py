497b13345e7d9bf9dbffb0aa1027877df2b87fbb
AlePezzelato <apezzelato1@gmail.com>
2024-05-27 17:53:01 -0300

    agregur el login y el logout

--- Escuela_CABJ/core/urls.py
+++ Escuela_CABJ/core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from . import views
+from  core.views import index, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index, name="index"),
+    path("login/", CustomLoginView.as_view(), name = "login"),
+    path("logout/", LogoutView.as_view(template_name = "core/logout.html"), name= "logout")
 ]
