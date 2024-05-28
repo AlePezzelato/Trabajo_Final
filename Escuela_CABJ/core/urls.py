f80f7241069c4c85e9f8241e5475b8b335487964
AlePezzelato <apezzelato1@gmail.com>
2024-05-28 10:58:13 -0300

    creacion de registro de usuario

--- Escuela_CABJ/core/urls.py
+++ Escuela_CABJ/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from  core.views import index, CustomLoginView
+from  core.views import index, CustomLoginView, registro
 from django.contrib.auth.views import LogoutView
 
 app_name = "core"
@@ -8,5 +8,7 @@ app_name = "core"
 urlpatterns = [
     path("", index, name="index"),
     path("login/", CustomLoginView.as_view(), name = "login"),
-    path("logout/", LogoutView.as_view(template_name = "core/logout.html"), name= "logout")
+    path("logout/", LogoutView.as_view(template_name = "core/logout.html"), name= "logout"),
+    path("registro/", registro, name= "registro"),
 ]
+
--- Escuela_CABJ/core/views.py
+++ Escuela_CABJ/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponse, HttpRequest
 
 def index(request):
     return render (request, "core/index.html")
@@ -8,3 +9,14 @@ def index(request):
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = "core/login.html"
+    
+def registro(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            return render(request, "core/index.html", {"mensaje": f"Usuario '{username}' creado"})
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "core/registro.html", {"form": form})
