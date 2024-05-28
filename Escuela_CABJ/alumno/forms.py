d96e967172274b3c0ecd8013963f0c7dde67fc01
AlePezzelato <apezzelato1@gmail.com>
2024-05-28 11:45:16 -0300

    modifique el forms de alumno

--- Escuela_CABJ/alumno/forms.py
+++ Escuela_CABJ/alumno/forms.py
@@ -6,3 +6,11 @@ class AlumnoForm(forms.ModelForm):
     class Meta:
         model = Alumno
         fields = ["nombre", "apellido", "fecha_de_nacimiento", "año_curso", "docente_id", "materia_id" ]
+        widgets = {
+            "nombre": forms.Select(attrs={"class": "form-control"}),
+            "apellido": forms.TextInput(attrs={"class": "form-control"}),
+            "gecha_de_nacimiento": forms.TextInput(attrs={"class": "form-control"}),
+            "año_curso": forms.TextInput(attrs={"class": "form-control"}),
+            "docente_id": forms.TextInput(attrs={"class": "form-control"}),
+            "materia_id": forms.TextInput(attrs={"class": "form-control"}),
+        }
