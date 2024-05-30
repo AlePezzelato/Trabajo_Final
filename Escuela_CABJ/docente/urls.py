d15a41c1ab582f4199ffac13f8721bd3f85a182a
AlePezzelato <apezzelato1@gmail.com>
2024-05-30 18:08:39 -0300

    agregue funcionalidades y html a la app docente

--- Escuela_CABJ/docente/urls.py
+++ Escuela_CABJ/docente/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from docente.views import listado_docentes, index, crear_docente
+from docente.views import listado_docentes, index, crear_docente, detalle_docentes, actualizar_docente, borrar_docente
 app_name = "docente"
 
 urlpatterns = [
     path("", index, name="index"),
     path("listado_docentes", listado_docentes, name="listado_docentes"),
     path("crear_docente", crear_docente, name="crear_docente"),
-    ]
+    path("detalle_docente<int:pk>", detalle_docentes, name="detalle_docente"),
+    path("actualizar_docente<int:pk>", actualizar_docente, name="actualizar_docente"),
+    path("borrar_docente<int:pk>", borrar_docente, name="borrar_docente"),
+]
+    
 
--- Escuela_CABJ/docente/views.py
+++ Escuela_CABJ/docente/views.py
@@ -8,9 +8,14 @@ def index(request):
 
 
 def listado_docentes(request):
-    consulta = Docente.objects.all()
-    contexto = {"docentes": consulta}
-    return render (request, "docente/listado_docentes.html", contexto)
+    busqueda = request.GET.get("busqueda", None)
+    if busqueda:
+        print(busqueda)
+        consulta = Docente.objects.filter(nombre__icontains=busqueda)
+    else:
+        consulta = Docente.objects.all()
+        contexto = {"docente": consulta}
+    return render(request, "docente/listado_docentes.html", contexto)
 
 def crear_docente(request):
     if request.method == "POST":
@@ -22,3 +27,25 @@ def crear_docente(request):
         form = DocenteForm()
     return render(request, "docente/crear_docente.html", {"form": form})
 
+def detalle_docentes(request, pk: int):
+    consulta = Docente.objects.get(id=pk)
+    contexto = {"docente": consulta}
+    return render (request, "docente/detalle_docentes.html", contexto)
+
+def actualizar_docente(request, pk: int):
+    consulta = Docente.objects.get(id=pk)
+    if request.method == "POST":
+        form = DocenteForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("docente:listado_docentes")
+    else:  # GET
+        form = DocenteForm(instance=consulta)
+    return render(request, "docente/crear_docente.html", {"form": form})
+
+def borrar_docente(request, pk: int):
+    consulta = Docente.objects.get(id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("docente:listado_docentes")
+    return render(request, "docente/confirma_borrar.html", {"object": consulta})
